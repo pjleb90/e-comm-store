68b4b3d95fdbbcec56705fa8c906dfbfcf3c3f1f
pjleb90 <pjleb90@gmail.com>
2023-09-23 11:08:24 -0400

    added featured products view to the home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("b741f48f-c899-4d0d-8bd1-a307d7ad0167");
+
   return (
-    <div>Hello store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
   )
-}
+};
 
 export default HomePage;
