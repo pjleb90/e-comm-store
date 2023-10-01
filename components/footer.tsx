01fb7eb1d9258f82bfe820f297fbb71cf3350b9a
pjleb90 <pjleb90@gmail.com>
2023-10-01 10:42:50 -0400

    minor text change

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 Store, Inc. All rights reserved.
+                    &copy; Emporium 2023, Inc. I do not own or take credit for any of the products contained. For development purposes only.
                 </p>
             </div>
         </footer>
