c1bccf60177a859973cd6343f0a3d32a82c3ae07
pjleb90 <pjleb90@gmail.com>
2023-11-13 19:13:11 -0500

    update footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,17 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; Emporium 2023, Inc. I do not own or take credit for any of the products contained. For development purposes only.
+                    Disclaimer: Thank you for visiting Emporium, the e-commerce store created for portfolio purposes.
+                    Please note that Emporium and all the items listed on this website are entirely fictional and intended solely
+                    for demonstration and showcase purposes.
+
+                    1 All products, descriptions, prices, and any other content related to items on Emporium are entirely fictional. They do not represent real products, services, or transactions.
+                    2 Emporium is a project designed to showcase the web development skills of the creator. It is not a functioning e-commerce platform, and no real transactions can be conducted on this website.
+                    3 This website does not operate as a real business. Any attempts to make purchases, transactions, or engage in any form of commercial activity on Emporium will not result in actual goods or services being provided.
+                    4 Emporium does not collect or store any real user data. Any information provided during the fictional checkout process is for demonstration purposes only and is not retained.
+                    5 The contact information, including email addresses or phone numbers, listed on this website is also fictional. Please do not attempt to contact Emporium for real business inquiries.
+
+                    By using this website, you acknowledge and agree to the fictional nature of Emporium and its content.
                 </p>
             </div>
         </footer>
